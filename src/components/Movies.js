f671afb8f86f9efe5a265fb449645053913e9f0d
Michael Sutton <hicallmesutton@gmail.com>
2021-03-09 00:39:38 -0600

    commit tree d2c06ef19dcbf558e64b5c737e7c7ad21e07f8e9

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { movies } from '../data';
 
+const listMovies = movies.map( movie =>
+  <div>
+    <h2>{movie.title}</h2>
+    <p>time {movie.time}</p>
+    <ul>
+      {movie.genres.map( genre => <li>{genre}</li>)}
+    </ul>
+  </div>
+)
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {listMovies}
     </div>
   );
 };
